bf7980e901805c082c01f36e2d8078c56a7950a5
Driouech-dev <59534645+Driouech-dev@users.noreply.github.com>
2026-04-07 12:29:17 +0800

    Update site URL and base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://luckypeptides.com',
+  site: 'https://driouech-dev.github.io',
+  base: '/pepprime/',
   integrations: [tailwind()],
 });
